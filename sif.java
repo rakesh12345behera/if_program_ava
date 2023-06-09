2f562675fa5bed8da59d0c77522c7aeb40121973
rakesh12345behera <134148231+rakesh12345behera@users.noreply.github.com>
2023-06-09 23:00:59 +0530

    java if program
    java program of password

---
+++ sif.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sif{
+public static void main(String[]args){
+int pwd;
+System.out.print("enter the possward:");
+Scanner obj=new Scanner(System.in);
+pwd=obj.nextInt();
+if(pwd==123)
+{
+System.out.println("Rakesh");
+System.out.println("22");
+}
+}
+}
